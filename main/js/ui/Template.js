e98e5f3b2d6b697bf7736fb0525cdeef14d5b3c8
Jens Riemschneider <kyr>
2012-11-20 08:16:01 +0100

    Using template to create ImageSelectionImageDiv

--- main/js/ui/Template.js
+++ main/js/ui/Template.js
@@ -10,43 +10,44 @@ net.riemschneider.ui = net.riemschneider.ui || {};
   net.riemschneider.ui.Template = {
     create: function create(templateId) {
       ArgumentUtils.assertString(templateId);
-	  
-	  var template = $('[data-template-id="' + templateId + '"]');
-	  ArgumentUtils.assertRange(template.length, 1, 1);
+
+      var template = $('[data-template-id="' + templateId + '"]');
+      ArgumentUtils.assertRange(template.length, 1, 1);
 
       return {
         clone: function clone(data) {
-		  ArgumentUtils.assertNotNull(data);
-          
-		  var clone = template.clone();
-		  var elements = {};
-		  var htmlElements = clone.find('[data-id]');
-		  for (var idx = 0, len = htmlElements.length; idx < len; ++idx) {
-		    var elem = $(htmlElements[idx]);
-			var key = elem.attr('data-id');
-		    if (data.hasOwnProperty(key)) {
-			  var value = data[key];
-			  var attr = elem.attr('data-attr');
-			  if (attr === 'text') {
-				elem.text(value);
-			  }
-			  else {
-				elem.attr(attr, value);
-			  }
+          ArgumentUtils.assertNotNull(data);
 
-			  elements[key] = elem;
-			}
-			else {
-			  elem.remove();
-			}
-		  }
-		  
-		  return {
-		    getClone: function getClone() { return clone; },
-			getElement: function getElement(elementId) { return elements[elementId]; }
-		  };
-		}
-	  };
+          var clonedElement = template.clone();
+          clonedElement.removeAttr('data-template-id');
+          var elements = {};
+          var htmlElements = clonedElement.find('[data-id]');
+          for (var idx = 0, len = htmlElements.length; idx < len; ++idx) {
+            var elem = $(htmlElements[idx]);
+            var key = elem.attr('data-id');
+            if (data.hasOwnProperty(key)) {
+              var value = data[key];
+              var attr = elem.attr('data-attr');
+              if (attr === 'text') {
+                elem.text(value);
+              }
+              else {
+                elem.attr(attr, value);
+              }
+
+              elements[key] = elem;
+            }
+            else {
+              elem.remove();
+            }
+          }
+
+          return {
+            getClone: function getClone() { return clonedElement; },
+            getElement: function getElement(elementId) { return elements[elementId]; }
+          };
+        }
+      };
     }
   };
 
